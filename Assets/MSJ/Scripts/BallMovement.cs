130879d53b8f288178766baec4f49acb752d0c56
tive1021 <tive1021@naver.com>
2024-10-15 19:16:09 +0900

    ball movement
    main scene으로 변경
    ball 연속 튕기기 해제

--- Assets/MSJ/Scripts/BallMovement.cs
+++ Assets/MSJ/Scripts/BallMovement.cs
@@ -9,6 +9,7 @@ public class BallMovement : MonoBehaviour
     private Rigidbody2D _rb2d;
     [SerializeField] private float speed = 1f;
     private Vector2 direction = new Vector2(1, 1).normalized;
+    private bool isCollider = false;
 
 
     // Start is called before the first frame update
@@ -25,11 +26,29 @@ public class BallMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // 예시: 벽과 충돌하면 반대 방향으로 이동
-        if (collision.gameObject.CompareTag("Brick"))
+        if (isCollider)
         {
-            direction = -direction;  // 방향 반전
+            return;
         }
+        isCollider = true;
+
+        Vector2 collisionNormal = collision.contacts[0].normal;
+
+        // 수직 충돌 (위쪽 또는 아래쪽 충돌)
+        if (Mathf.Abs(collisionNormal.y) > Mathf.Abs(collisionNormal.x))
+        {
+            direction.y = -direction.y;  // y축 반전 (수직 반전)
+        }
+        // 수평 충돌 (왼쪽 또는 오른쪽 충돌)
+        else if (Mathf.Abs(collisionNormal.x) > Mathf.Abs(collisionNormal.y))
+        {
+            direction.x = -direction.x;  // x축 반전 (수평 반전)
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isCollider = false;
     }
 
 
