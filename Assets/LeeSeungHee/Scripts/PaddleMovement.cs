559a0e0832adcff95b2314ff7b1db3e76a75e0d3
lsw7791 <lsw7791@naver.com>
2024-10-15 19:42:52 +0900

    Update

---
+++ Assets/LeeSeungHee/Scripts/PaddleMovement.cs
@@ -0,0 +1,29 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D move_rb;
+
+    private float moveDirection;
+
+    private void Start()
+    {
+        move_rb = GetComponent<Rigidbody2D>();
+        move_rb.gravityScale = 0; 
+        move_rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY; 
+    }
+
+    private void Update()
+    {
+        moveDirection = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        move_rb.velocity = new Vector2(moveDirection * moveSpeed, 0);
+    }
+}
--- Assets/MSJ/Scripts/BallMovement.cs
+++ Assets/MSJ/Scripts/BallMovement.cs
@@ -1,24 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class BallMovement : MonoBehaviour
 {
     private Rigidbody2D _rb2d;
     [SerializeField] private float speed = 1f;
     private Vector2 direction = new Vector2(1, 1).normalized;
-    private bool isCollider = false;
 
-
-    // Start is called before the first frame update
     void Awake()
     {
         _rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         _rb2d.velocity = direction * speed;
@@ -26,30 +20,7 @@ public class BallMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (isCollider)
-        {
-            return;
-        }
-        isCollider = true;
-
         Vector2 collisionNormal = collision.contacts[0].normal;
-
-        // 수직 충돌 (위쪽 또는 아래쪽 충돌)
-        if (Mathf.Abs(collisionNormal.y) > Mathf.Abs(collisionNormal.x))
-        {
-            direction.y = -direction.y;  // y축 반전 (수직 반전)
-        }
-        // 수평 충돌 (왼쪽 또는 오른쪽 충돌)
-        else if (Mathf.Abs(collisionNormal.x) > Mathf.Abs(collisionNormal.y))
-        {
-            direction.x = -direction.x;  // x축 반전 (수평 반전)
-        }
+        direction = Vector2.Reflect(direction, collisionNormal).normalized; 
     }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        isCollider = false;
-    }
-
-
 }
