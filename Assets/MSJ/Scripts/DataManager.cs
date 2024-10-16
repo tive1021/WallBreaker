d53d7155ab0d2a84f5d823f0019c70c2d362a7fc
tive1021 <tive1021@naver.com>
2024-10-16 10:37:16 +0900

    playerprefs 추가

--- Assets/MSJ/Scripts/DataManager.cs
+++ Assets/MSJ/Scripts/DataManager.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
+    private string currentScore = "CurrentScore";
+    private string highScore = "HighScore";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!PlayerPrefs.HasKey(highScore))
+        {
+            PlayerPrefs.SetInt(highScore, 0);
+        }
+        PlayerPrefs.SetInt(currentScore, 0);
     }
 
     // Update is called once per frame
