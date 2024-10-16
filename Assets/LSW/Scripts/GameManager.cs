338f8655aa96fbc7aee5cf5164a0879b1dcab54f
lsw7791 <lsw7791@naver.com>
2024-10-16 10:36:24 +0900

    Singleton

--- Assets/LSW/Scripts/GameManager.cs
+++ Assets/LSW/Scripts/GameManager.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
     public GameObject wallPrefab; 
     public Vector2 startPosition = new Vector2(-2.5f, 4.3f); 
     public int columns = 9; 
     public int rows = 5; 
     public float xSpacing = 0.6f; 
     public float ySpacing = 0.3f;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject); 
+        }
+    }
 
     void Start()
     {
